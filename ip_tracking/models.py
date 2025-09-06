596332442c2b79fdc36bc6fea8c8e26af7eb91cd
therushmorare <tshego.tt.morare@gmail.com>
2025-09-07 01:17:53 +0200

    task 1

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -10,3 +10,8 @@ class RequestLog(models.Model):
     def __str__(self):
         return f"{self.ip_address} - {self.path} @ {self.timestamp}"
 
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return f"Blocked: {self.ip_address}"
